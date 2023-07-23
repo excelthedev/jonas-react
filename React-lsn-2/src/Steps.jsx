64609325f6dcdbbcacc20c7f501b26665da48e56
theseventhgod <tobyadeyy@gmail.com>
2023-07-23 04:36:34 +0100

    still on states

---
+++ React-lsn-2/src/Steps.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+const Steps = () => {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) setStep((s) => s + 1);
+  }
+
+  function handleIsOpen() {
+    setIsOpen((is) => !is);
+    //setIsOpen(isOpen ? false : true);
+    //same logic , but one has been shortened and optimised for better performance
+    //notice our use of callback functions for passing of states
+  }
+
+  return (
+    <div>
+      <button className="close" onClick={handleIsOpen}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
+
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}{" "}
+    </div>
+  );
+};
+
+export default Steps;
